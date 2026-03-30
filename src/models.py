a90f80df87cebebb8f34cb56ad1eabb8d134978a
thawancomt <thawancomt@gmail.com>
2026-03-30 17:27:07 +0100

    fix: Fix function call in User model for updated_at column

--- src/models.py
+++ src/models.py
@@ -6,14 +6,19 @@ from datetime import datetime
 from src.db.database import Base
 
 
-
 import uuid
-class User(Base):
-     __tablename__ = 'users'
 
-     uid: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True, default=uuid.uuid4)
-     username: Mapped[str] = mapped_column(String(30), nullable=False)
-     email: Mapped[str] = mapped_column(String(255), nullable=False)
-     password_hash: Mapped[str] = mapped_column(Text, nullable=False)
-     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
-     updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now, onupdate=func.now())
+
+class User(Base):
+    __tablename__ = "users"
+
+    uid: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True, default=uuid.uuid4)
+    username: Mapped[str] = mapped_column(String(30), nullable=False)
+    email: Mapped[str] = mapped_column(String(255), nullable=False)
+    password_hash: Mapped[str] = mapped_column(Text, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+    )
