1ae660a8272e61440290e0e3cca077bd3fbe4780
Davide Masola <davide.masola@gmail.com>
2026-04-01 16:40:38 +0200

    fix: sanitize validation errors to hide sensitive input

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
 from src.db.database import engine
 from src.models import Base
@@ -34,4 +36,18 @@ async def root():
 if settings.DEBUG:
     app.add_middleware(
         CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
+    )
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    
+    errors = exc.errors()
+    for error in errors:
+        if "input" in error:
+            del error["input"]
+            
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content={"detail": errors},
     )
